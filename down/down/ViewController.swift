9af1913fd427cee15ba5eadfa7282e25b3c034a4
Suraj Sinha <ssinha10@illinois.edu>
2016-12-20 18:39:33 -0600

    firebase login works!

--- down/down/ViewController.swift
+++ down/down/ViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var logoutButton: UIButton!
 
+    @IBOutlet weak var usernameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.hideKeyboardWhenTappedAround()
+        
+        if let user = FIRAuth.auth()?.currentUser {
+            
+            self.logoutButton.alpha = 1.0
+            self.usernameLabel.text = user.email
+            
+        } else {
+            self.logoutButton.alpha = 0.0
+            self.usernameLabel.text = ""
+            
+        }
         
         
         
@@ -24,6 +40,72 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func createAccountAction(_ sender: Any) {
+        if self.emailField.text == "" || self.passwordField.text == "" {
+            let alertController = UIAlertController(title: "Oops", message: "Please enter an email and password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            FIRAuth.auth()?.createUser(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: { (user, error) in
+                if error == nil {
+                    self.logoutButton.alpha = 1.0
+                    self.usernameLabel.text = user?.email
+                    self.emailField.text = ""
+                    self.passwordField.text = ""
+                    
+                }
+                else {
+                    let alertController = UIAlertController(title: "Oops", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            })
+        }
+        
+        
+    }
+    @IBAction func loginAction(_ sender: Any) {
+        if self.emailField.text == "" || self.passwordField.text == "" {
+            let alertController = UIAlertController(title: "Oops", message: "Please enter an email and password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            FIRAuth.auth()?.signIn(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: { (user, error) in
+                if error == nil {
+                    self.logoutButton.alpha = 1.0
+                    self.usernameLabel.text = user?.email
+                    self.emailField.text = ""
+                    self.passwordField.text = ""
+                    
+                }
+                else {
+                    let alertController = UIAlertController(title: "Oops", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+
+            })
+        }
+        
+        
+    }
+    @IBAction func logoutAction(_ sender: Any) {
+        
+        try! FIRAuth.auth()?.signOut()
+        self.usernameLabel.text = ""
+        self.logoutButton.alpha = 0.0
+        self.passwordField.text = ""
+        
+        
+        
+        
+    }
 
 }
 
